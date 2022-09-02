1d1543d4c3a8baa2e34040b14f16ea73c3a3f5d7
hung <hungtrewq3214@gmail.com>
2022-09-02 19:52:00 +0700

    Xây dựng UI phần sidebar #3

--- src/components/SuggestedAccounts/AccountItem.js
+++ src/components/SuggestedAccounts/AccountItem.js
@@ -2,29 +2,46 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Tippy from '@tippyjs/react/headless';
 
 import styles from './SuggestedAccounts.module.scss';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import Image from '../Image';
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+import AccountPreview from './AccountPreview';
 
 const cx = classNames.bind(styles);
 
 function AccountItem() {
-    return (
-        <div className={cx('account-item')}>
-            <img
-                className={cx('avatar')}
-                src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-aiso/65d3c6b1d1e205c75536ccf1f26d552d~c5_100x100.jpeg?x-expires=1660910400&x-signature=5uAXqNlYbUYiKm6eoIbcqEtIZ28%3D"
-                alt=""
-            />
-
-            <div className={cx('item-info')}>
-                <p className={cx('nickname')}>
-                    <strong>nguyenkimhung</strong>
-                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
-                </p>
-
-                <p className={cx('name')}>Nguyễn Kim Hưng</p>
+    const renderPreview = (props) => {
+        return (
+            <div tabIndex="-1" {...props}>
+                <PopperWrapper>
+                    <AccountPreview />
+                </PopperWrapper>
             </div>
+        );
+    };
+    return (
+        <div>
+            <Tippy interactive delay={[800, 0]} offset={[-20, 0]} placement="bottom" render={renderPreview}>
+                <div className={cx('account-item')}>
+                    <Image
+                        className={cx('avatar')}
+                        src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-aiso/65d3c6b1d1e205c75536ccf1f26d552d~c5_100x100.jpeg?x-expires=1660910400&x-signature=5uAXqNlYbUYiKm6eoIbcqEtIZ28%3D"
+                        alt=""
+                    />
+
+                    <div className={cx('item-info')}>
+                        <p className={cx('nickname')}>
+                            <strong>nguyenkimhung</strong>
+                            <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                        </p>
+
+                        <p className={cx('name')}>Nguyễn Kim Hưng</p>
+                    </div>
+                </div>
+            </Tippy>
         </div>
     );
 }
